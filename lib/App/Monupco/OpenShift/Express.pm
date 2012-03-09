ee1dd2c78c54d2fa92c848cf7f0cf5fe4d8deecf
Alexander Todorov <atodorov@otb.bg>
2012-03-09 13:48:00 +0200

    add more text to POD

--- lib/App/Monupco/OpenShift/Express.pm
+++ lib/App/Monupco/OpenShift/Express.pm
@@ -31,7 +31,7 @@ my $data = {
 };
 
 my $pod_parsed = "";
-my $parser = Monupco::OpenShift::Express::Parser->new();
+my $parser = App::Monupco::OpenShift::Express::Parser->new();
 $parser->output_string( \$pod_parsed );
 $parser->parse_file("$ENV{'OPENSHIFT_DATA_DIR'}/perl5lib/lib/perl5/x86_64-linux-thread-multi/perllocal.pod");
 
@@ -66,11 +66,42 @@ __END__
 
 =head1 NAME
 
-Monupco::OpenShift::Express - monupco.com registration agent for OpenShift Express / Perl applications
+App::Monupco::OpenShift::Express - monupco.com registration agent for OpenShift Express / Perl applications
 
 =head1 SYNOPSIS
 
-  use Monupco::OpenShift::Express;
+To register your OpenShift Perl application to Monupco do the following:
+
+1) Create a Perl application on OpenShift:
+
+    rhc-create-app -a myapp -t perl-5.10
+
+2) Add a dependency in your deplist.txt file
+
+    cd ./myapp/
+    echo "App::Monupco::OpenShift::Express" >> deplist.txt
+
+3) Set your username and application name in the ./data/MONUPCO_SETTINGS file
+
+    echo "export MONUPCO_USER_ID=YourUserID"  > ./data/MONUPCO_SETTINGS
+
+4) Enable the registration script in .openshift/action_hooks/post_deploy
+
+    source $OPENSHIFT_REPO_DIR/data/MONUPCO_SETTINGS
+    $OPENSHIFT_APP_DIR/perl5lib/lib/perl5/App/Monupco/OpenShift/Express.pm
+
+5) Commit your changes
+
+    git add .
+    git commit -m "enable monupco registration"
+
+6) Then push your application to OpenShift
+
+    git push
+
+That's it, you can now check your application statistics at
+http://monupco.com
+
 
 =head1 DESCRIPTION
 
